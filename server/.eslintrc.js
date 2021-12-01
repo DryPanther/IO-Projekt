d8cd01991f7a74e6167b7aa29f1e6200785c5a1c
Manuel Hływa <manfredzik14@gmail.com>
2021-12-01 23:49:05 +0100

    Refactoring (od zera)
    Całkowita przebudowa strony z użyciem  Vue.js & Express.js
    
    Co-Authored-By: Artur Bąk <92635739+Aker212@users.noreply.github.com>
    Co-Authored-By: Michał Cader <92511302+Cad1x@users.noreply.github.com>
    Co-Authored-By: Jakub Błachut <92635745+KERIOSS@users.noreply.github.com>

---
+++ server/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": "plugin:vue/essential",
+    "parserOptions": {
+        "ecmaVersion": 13
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+    }
+};
