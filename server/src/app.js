500c794b29db4378ec821c1e33343cb2258767b8
Manuel Hływa <manfredzik14@gmail.com>
2021-12-08 22:04:08 +0100

    Połączenie z bazą danych
    Połączenie z bazą danych mongodb

--- server/src/app.js
+++ server/src/app.js
@@ -1,18 +1,39 @@
 const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
 const morgan = require('morgan')
+const createError = require('http-errors')
+require('dotenv').config()
+require('./helpers/init_mongodb')
+
+const AuthRoute = require('./Routes/Auth.route')
 
 const app = express()
-app.use(morgan('combined'))
-app.use(bodyParser.json())
-app.use(cors())
+app.use(morgan('dev'))
 
-app.post('/register',(req,res) =>{
-res.send({
-    message: `Hello ${req.body.email}!your user was registered! Have fun!`,
-
-})
+app.get('/', async (req, res, next) => {
+    res.send("Hello from express")
 })
 
-app.listen(process.env.PORT || 8081)
+app.use('/auth', AuthRoute)
+
+app.use(async(req, res, next) => {
+    //const error = new Error("Not found")
+    //error.status = 404
+    //next(error)
+    next(createError.NotFound())
+})
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500)
+    res.send({
+        error: {
+            status: err.status || 500,
+            message: err.message,
+        }
+    })
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
