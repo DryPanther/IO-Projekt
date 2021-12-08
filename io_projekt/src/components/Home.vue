98b8f0f076621bc8a38e896f7dcd90e8602daf29
Cad1x <michcad123@gmail.com>
2021-12-08 19:24:13 +0100

    Strona główna/pasek nawigacyjny
    Dodałem stronę główną, oraz funkcjonalny pasek nawigacyjny

---
+++ io_projekt/src/components/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+  <div id="nawigacja">
+<nav class="navbar navbar-expand-lg navbar-dark ">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/#/home">IO_Projekt</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/#/home">Strona Główna</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/#/login">Zaloguj</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/#/register">Zarejestruj</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Szukaj" aria-label="Szukaj">
+        <button class="btn btn-light" type="submit">Szukaj</button>
+      </form>
+    </div>
+  </div>
+</nav>
+</div>
+    
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+#nawigacja{
+  margin-top:-60px;
+  z-index: 9999;
+  background:#59320A;
+  position:fixed;
+  width:100%;
+}
+</style>
