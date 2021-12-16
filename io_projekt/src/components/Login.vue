04b25d4e25e788bfad1a1098c98cd60a35b959e0
Manuel Hływa <manfredzik14@gmail.com>
2021-12-16 09:41:46 +0100

    Poprawa hiperlinków

--- io_projekt/src/components/Login.vue
+++ io_projekt/src/components/Login.vue
@@ -14,6 +14,7 @@
     <label for="exampleInputPassword1" clabss="form-label">Hasło</label>
     <input type="password" class="form-control" id="exampleInputPassword1">
   </div>
+  <a class="nav-link" href="/#/forgot">Nie pamiętam hasła</a>
   <div id="check" class="mb-3 form-check">
     <input type="checkbox" class="form-check-input" id="exampleCheck1">
     <label class="form-check-label" for="exampleCheck1">Check me out</label>
--- io_projekt/src/components/navigation.vue
+++ io_projekt/src/components/navigation.vue
@@ -18,6 +18,9 @@
         <li class="nav-item">
           <a class="nav-link" href="/#/register">Zarejestruj</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/#/kategorie">Kategorie</a>
+        </li>
       </ul>
       <form class="d-flex">
         <input class="form-control me-2" type="search" placeholder="Szukaj" aria-label="Szukaj">
